9f8fdbc238cf6404a6afb5c6ee8438a7f4a0d8d9
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-06 22:23:52 +0000

    Verify, test and fix bug in index.js for announceDawn method.

--- test/index_test.js
+++ test/index_test.js
@@ -3,6 +3,13 @@ const Rooster = require("../index");
 
 describe("Rooster", () => {
   describe(".announceDawn", () => {
-    it("returns a rooster call", () => {});
+    it("returns a rooster call", () => {
+      //setup
+      const expected = "cock-a-doodle-doo!";
+      //exercise
+      const result = Rooster.announceDawn();
+      //verify
+      assert.strictEqual(result, expected);
+    });
   });
 });
