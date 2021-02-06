1b7d2815c9fdf496d25fc7adbb66adf9f82d18b8
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-06 22:42:51 +0000

    Verify, and test for timeAtDawn method - no bugs found.

--- test/index_test.js
+++ test/index_test.js
@@ -7,9 +7,43 @@ describe("Rooster", () => {
       //setup
       const expected = "cock-a-doodle-doo!";
       //exercise
-      const result = Rooster.announceDawn();
+      const actual = Rooster.announceDawn();
       //verify
-      assert.strictEqual(result, expected);
+      assert.strictEqual(actual, expected);
+    });
+  });
+
+  describe(".timeAtDawn", () => {
+    it("returns its argument as a string", () => {
+      //setup
+      const inputNumber = 6;
+      const expected = "6";
+      //exercise
+      const actual = Rooster.timeAtDawn(inputNumber);
+      //verify
+      assert.strictEqual(actual, expected);
+    });
+
+    it("throws range error if given number is less than 0", () => {
+      //setup
+      const inputNumber = -1;
+      const expected = RangeError();
+
+      //verify
+      assert.throws(() => {
+        Rooster.timeAtDawn(inputNumber); //exercise in callback
+      }, expected);
+    });
+
+    it("throws range error if given number is more than 23", () => {
+      //setup
+      const inputNumber = 24;
+      const expected = RangeError();
+
+      //verify
+      assert.throws(() => {
+        Rooster.timeAtDawn(inputNumber); //exercise in callback
+      }, expected);
     });
   });
 });
